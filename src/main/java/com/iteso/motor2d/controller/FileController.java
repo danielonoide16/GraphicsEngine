798274dcecd95bff550434b9c9ff2148741c837c
Alex9112 <encisocris0420@gmail.com>
2025-12-01 11:18:47 -0600

    Comentarios para file Controller

--- src/main/java/com/iteso/motor2d/controller/FileController.java
+++ src/main/java/com/iteso/motor2d/controller/FileController.java
@@ -1,20 +1,22 @@
 package com.iteso.motor2d.controller;
 
+// Dependencias para mainpular JSON
 import javax.json.Json;
 import javax.json.JsonObject;
 import javax.json.JsonReader;
 
+// Dependencias internas
 import com.iteso.motor2d.io.SceneReader;
 import com.iteso.motor2d.model.shapes.Shape2D;
-import com.iteso.motor2d.view.MainWindow;
 
+// Dependencias de la API especificas
 import java.io.FileInputStream;
 import java.io.InputStream;
 import java.util.List;
 import java.io.FileNotFoundException;
 
 public class FileController{
-    private String path;
+    private String path; // Ruta para leer el documento
     private JsonObject jsonShape;
 
     public FileController(String path){
@@ -22,6 +24,7 @@ public class FileController{
         generateFigures();
     }
 
+    // Funcion que lee el json apartir del path
     private void readJson(){
         try{
             InputStream file = new FileInputStream(this.path);
@@ -33,6 +36,8 @@ public class FileController{
             System.out.println("No se encuentra el error");
         };
     }
+
+    // Funcion que genera una lista de shapes2D apartir del json leido
     public List<Shape2D> generateFigures(){
         readJson();
         SceneReader lector = new SceneReader(jsonShape);
